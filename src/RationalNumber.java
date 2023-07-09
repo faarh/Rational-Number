980d9c8265e0a109bda64d72902b59a63204550b
Faarha <Faarha@gmail.com>
2023-07-09 17:02:18 +0500

    intilizied the numerator and denominators and a default constructor

---
+++ src/RationalNumber.java
@@ -0,0 +1,11 @@
+package src;
+
+public class RationalNumber {
+    private int numerator;
+    private int denominator;
+
+    public RationalNumber() {
+        numerator = 0;
+        denominator = 1;
+    }
+}
